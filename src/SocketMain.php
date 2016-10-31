6ff18f8f4865a3162830e89b52977189bba16064
Gert <Gert-dev@users.noreply.github.com>
2016-10-31 20:36:31 +0100

    Move class

--- src/SocketMain.php
+++ src/SocketMain.php
@@ -5,6 +5,6 @@ require 'Bootstrap.php';
 echo "Starting socket server...\n";
 
 $loop = React\EventLoop\Factory::create();
-$socket = new PhpIntegrator\SocketServer($loop, 9999);
+$socket = new PhpIntegrator\Sockets\SocketServer($loop, 9999);
 
 $loop->run();
