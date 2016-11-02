247fa77882ea36e8352ca3be332b695fb60d1819
Gert <Gert-dev@users.noreply.github.com>
2016-11-02 19:37:59 +0100

    Combine SocketMain and Main

--- src/Main.php
+++ src/Main.php
@@ -1,5 +1,35 @@
 <?php
 
+use PhpIntegrator\Sockets\ConnectionHandlerFactory;
+
+use PhpIntegrator\UserInterface\JsonRpcApplication;
+
 require 'Bootstrap.php';
 
-echo (new \PhpIntegrator\UserInterface\CliApplication())->handleCommandLineArguments($argv);
+$options = getopt('p:', [
+    'server'
+]);
+
+if (!isset($options['server'])) {
+    echo (new \PhpIntegrator\UserInterface\CliApplication())->handleCommandLineArguments($argv);
+    return;
+}
+
+if (!isset($options['p'])) {
+    die('A port must be passed in order to run in socket server mode');
+}
+
+echo "Starting socket server on port {$options['p']}...\n";
+
+$stdinStream = fopen('php://memory', 'w+');
+
+$applicationJsonRpcRequestHandler = new JsonRpcApplication($stdinStream);
+
+$connectionHandlerFactory = new ConnectionHandlerFactory($applicationJsonRpcRequestHandler);
+
+$loop = React\EventLoop\Factory::create();
+$socket = new PhpIntegrator\Sockets\SocketServer($loop, $options['p'], $connectionHandlerFactory);
+
+$loop->run();
+
+fclose($stdinStream);
