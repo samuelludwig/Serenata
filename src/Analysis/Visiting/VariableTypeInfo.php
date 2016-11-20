68f42d9a3ba505259d541d3ffa49544dfe611012
Gert <Gert-dev@users.noreply.github.com>
2016-11-20 20:16:26 +0100

    Add documentation to VariableTypeInfo

--- src/Analysis/Visiting/VariableTypeInfo.php
+++ src/Analysis/Visiting/VariableTypeInfo.php
@@ -10,21 +10,48 @@ use PhpParser\Node;
 class VariableTypeInfo
 {
     /**
+     * The node that best describes the item.
+     *
+     * This is usually a node such as an assignment expression or a method parameter that the item is involved in. For
+     * example, if a node "$a = $b" is encountered, followed by a node "$a = $c", then the best match for the type of
+     * $a would be the node that describes "$a = $c".
+     *
      * @var Node|null
      */
     protected $bestMatch;
 
     /**
+     * Describes an override of the type.
+     *
+     * Type overrides are usually present in inline docblocks that override the type. This field would be filled with
+     * that type (override) that is set for this item. For example, "/** @var Foo $a * /" describes the type of $a to
+     * be overridden to be "Foo".
+     *
      * @var string|null
      */
     protected $bestTypeOverrideMatch;
 
     /**
+     * The line to type override was encountered at.
+     *
      * @var int|null
      */
     protected $bestTypeOverrideMatchLine;
 
     /**
+     * A map of conditional types that the item may have.
+     *
+     * Whenever the item is encountered inside a conditional (if statement, ternary expression, ...), there are certain
+     * assumptions that can be made about the type. For example, a check such as "if ($a === null)" clearly states that
+     * if the condition passes, the type of the expression must be null. At that point it doesn't matter if the type
+     * could previously be "Foo|null", as the conditional has now effectively limited the possible types.
+     *
+     * Conditional types can be:
+     *   - Guaranteed - For example, in "if ($a === null)", the type of $a is guaranteed to be "null".
+     *   - Possible   - For example, in "if (!$a)", the type of $a is possibly "null", "int" (with value 0), "string"
+     *                  (empty value), ...
+     *   - Impossible - For example, in "if ($a !== null)", the type of $a could never possibly be "null".
+     *
      * @var array
      */
     protected $conditionalTypes = [];
