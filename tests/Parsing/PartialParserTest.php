08391a4880fb80698d73610926a7516a76b8d83b
Gert <Gert-dev@users.noreply.github.com>
2017-02-02 19:57:52 +0100

    Fix parsing method calls as interpolated string values
    References #83

--- tests/Parsing/PartialParserTest.php
+++ tests/Parsing/PartialParserTest.php
@@ -524,6 +524,25 @@ SOURCE;
     /**
      * @return void
      */
+    public function testGetLastNodeAtCorrectlyDealsWithEncapsedStringWithIntepolatedMethodCall()
+    {
+        $source = <<<'SOURCE'
+            <?php
+
+            "{$test->foo()}"
+SOURCE;
+
+        $result = $this->createPartialParser()->getLastNodeAt($source);
+
+        $this->assertInstanceOf(Node\Scalar\Encapsed::class, $result);
+        $this->assertInstanceOf(Node\Expr\MethodCall::class, $result->parts[0]);
+        $this->assertEquals('test', $result->parts[0]->var->name);
+        $this->assertEquals('foo', $result->parts[0]->name);
+    }
+
+    /**
+     * @return void
+     */
     public function testGetLastNodeAtCorrectlyDealsWithNowdoc()
     {
         $source = <<<'SOURCE'
