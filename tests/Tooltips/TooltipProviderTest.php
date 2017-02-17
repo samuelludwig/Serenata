ece940dbfec7a87fbe3c4b0621ace494031bbfe5
Gert <Gert-dev@users.noreply.github.com>
2017-02-17 19:34:40 +0100

    Add function tooltip tests

---
+++ tests/Tooltips/TooltipProviderTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace PhpIntegrator\Tests\Tooltips;
+
+use PhpIntegrator\Tests\IndexedTest;
+
+use PhpIntegrator\Tooltips\TooltipResult;
+
+class TooltipProviderTest extends IndexedTest
+{
+    /**
+     * @param string $file
+     * @param int    $position
+     *
+     * @return TooltipResult|null
+     */
+    protected function getTooltip(string $file, int $position): ?TooltipResult
+    {
+        $path = $this->getPathFor($file);
+
+        $container = $this->createTestContainer();
+
+        $this->indexTestFile($container, $path);
+
+        $code = $container->get('sourceCodeStreamReader')->getSourceCodeFromFile($path);
+
+        return $container->get('tooltipProvider')->get($code, $position);
+    }
+
+    /**
+     * @param string $file
+     *
+     * @return string
+     */
+    protected function getPathFor(string $file): string
+    {
+        return __DIR__ . '/TooltipProviderTest/' . $file;
+    }
+
+    /**
+     * @return void
+     */
+    public function testGlobalFunction(): void
+    {
+        $fileName = 'GlobalFunction.phpt';
+
+        $result = $this->getTooltip($fileName, 411);
+
+        $this->assertNotNull($result);
+        $this->assertNull($result->getRange());
+        $this->assertEquals($result->getContents(), 'Hi! *Bold text* **Italic** ~~Strikethrough~~
+
+# Description
+## Header
+Hello!
+
+# Parameters
+   |   |   ' . '
+--- | --- | ---
+**• $first** | *string* | Testdescription
+**• $second** | *int* | Test
+**• $third** | *\Exception* | Test
+
+# Returns
+*void*
+
+# Throws
+   |   |   ' . '
+--- | --- | ---
+• **\Exception** | When something happens
+• **\LogicException** | When something else happens.
+• **\RuntimeException** |  ');
+    }
+}
---
+++ tests/Tooltips/TooltipProviderTest/GlobalFunction.phpt
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Hi! *Bold text* **Italic** ~~Strikethrough~~
+ *
+ * ## Header
+ * Hello!
+ *
+ * @param string     $first  Testdescription
+ * @param int        $second Test
+ * @param \Exception $third Test
+ *
+ * @throws \Exception      When something happens
+ * @throws \LogicException When something else happens.
+ * @throws \RuntimeException
+ */
+function test(string $first, int $second, \Exception $third)
+{
+
+}
+
+test();
