713e620e9ef8ca333c35b1befb86aeeddfd7f5d3
Gert <Gert-dev@users.noreply.github.com>
2017-03-26 14:39:08 +0200

    Add public access modifier

--- src/Utility/SpecialDocblockType.php
+++ src/Utility/SpecialDocblockType.php
@@ -10,90 +10,90 @@ class SpecialDocblockType
     /**
      * @var string
      */
-    const STRING_   = 'string';
+    public const STRING_   = 'string';
 
     /**
      * @var string
      */
-    const INT_      = 'int';
+    public const INT_      = 'int';
 
     /**
      * @var string
      */
-    const BOOL_     = 'bool';
+    public const BOOL_     = 'bool';
 
     /**
      * @var string
      */
-    const FLOAT_    = 'float';
+    public const FLOAT_    = 'float';
 
     /**
      * @var string
      */
-    const OBJECT_   = 'object';
+    public const OBJECT_   = 'object';
 
     /**
      * @var string
      */
-    const MIXED_    = 'mixed';
+    public const MIXED_    = 'mixed';
 
     /**
      * @var string
      */
-    const ARRAY_    = 'array';
+    public const ARRAY_    = 'array';
 
     /**
      * @var string
      */
-    const RESOURCE_ = 'resource';
+    public const RESOURCE_ = 'resource';
 
     /**
      * @var string
      */
-    const VOID_     = 'void';
+    public const VOID_     = 'void';
 
     /**
      * @var string
      */
-    const NULL_     = 'null';
+    public const NULL_     = 'null';
 
     /**
      * @var string
      */
-    const CALLABLE_ = 'callable';
+    public const CALLABLE_ = 'callable';
 
     /**
      * @var string
      */
-    const FALSE_    = 'false';
+    public const FALSE_    = 'false';
 
     /**
      * @var string
      */
-    const TRUE_     = 'true';
+    public const TRUE_     = 'true';
 
     /**
      * @var string
      */
-    const SELF_     = 'self';
+    public const SELF_     = 'self';
 
     /**
      * @var string
      */
-    const STATIC_   = 'static';
+    public const STATIC_   = 'static';
 
     /**
      * @var string
      */
-    const PARENT_   = 'parent';
+    public const PARENT_   = 'parent';
 
     /**
      * @var string
      */
-    const THIS_     = '$this';
+    public const THIS_     = '$this';
 
     /**
      * @var string
      */
-    const ITERABLE_ = 'iterable';
+    public const ITERABLE_ = 'iterable';
 }
