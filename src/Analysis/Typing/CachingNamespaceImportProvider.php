c4850dd5ddd470b64137519c7881bee4b768fcb1
Gert <Gert-dev@users.noreply.github.com>
2017-04-08 21:23:27 +0200

    Add caching layer to namespaceImportProvider

---
+++ src/Analysis/Typing/CachingNamespaceImportProvider.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace PhpIntegrator\Analysis\Typing;
+
+use Doctrine\Common\Cache\Cache;
+
+use PhpIntegrator\Analysis\ClearableCacheInterface;
+
+/**
+ * Namespace import provider that delegates its functionality to another object and adds a caching layer on top of it.
+ */
+class CachingNamespaceImportProvider implements NamespaceImportProviderInterface, ClearableCacheInterface
+{
+    /**
+     * @var NamespaceImportProviderInterface
+     */
+    private $delegate;
+
+    /**
+     * @var array
+     */
+    private $cache;
+
+    /**
+     * @param NamespaceImportProviderInterface $delegate
+     */
+    public function __construct(NamespaceImportProviderInterface $delegate)
+    {
+        $this->delegate = $delegate;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getNamespacesForFile(string $filePath): array
+    {
+        $id = __METHOD__ . $filePath;
+
+        if (!isset($this->cache[$id])) {
+            $this->cache[$id] = $this->delegate->getNamespacesForFile($filePath);
+        }
+
+        return $this->cache[$id];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUseStatementsForFile(string $filePath): array
+    {
+        $id = __METHOD__ . $filePath;
+
+        if (!isset($this->cache[$id])) {
+            $this->cache[$id] = $this->delegate->getUseStatementsForFile($filePath);
+        }
+
+        return $this->cache[$id];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getNamespaces(): array
+    {
+        $id = __METHOD__ . $filePath;
+
+        if (!isset($this->cache[$id])) {
+            $this->cache[$id] = $this->delegate->getNamespaces();
+        }
+
+        return $this->cache[$id];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function clearCache(): void
+    {
+        $this->cache = [];
+    }
+}
