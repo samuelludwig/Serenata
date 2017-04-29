b2f3702312ec7cc5bd35b4018156659d24548f42
Gert <Gert-dev@users.noreply.github.com>
2017-04-29 16:44:45 +0200

    Extract methods

--- src/Analysis/Visiting/UseStatementFetchingVisitor.php
+++ src/Analysis/Visiting/UseStatementFetchingVisitor.php
@@ -52,43 +52,9 @@ class UseStatementFetchingVisitor extends NodeVisitorAbstract
         if ($node instanceof Node\Stmt\Namespace_) {
             // There is no way to fetch the end of a namespace, so determine it manually (a value of null signifies the
             // end of the file).
-            $this->namespaces[$this->lastNamespaceIndex]['endLine'] = $node->getLine() - 1;
-            $this->namespaces[++$this->lastNamespaceIndex] = [
-                'name'          => $node->name ? (string) $node->name : null,
-                'startLine'     => $node->getLine(),
-                'endLine'       => $node->getLine() + 1,
-                'useStatements' => []
-            ];
+            $this->beginNamespace($node);
         } elseif ($node instanceof Node\Stmt\Use_ || $node instanceof Node\Stmt\GroupUse) {
-            $prefix = '';
-
-            if ($node instanceof Node\Stmt\GroupUse) {
-                $prefix = ((string) $node->prefix) . '\\';
-            };
-
-            foreach ($node->uses as $use) {
-                $type = $node->type === Node\Stmt\Use_::TYPE_UNKNOWN ? $use->type : $node->type;
-
-                if ($type === Node\Stmt\Use_::TYPE_UNKNOWN) {
-                    throw new DomainException('Unknown use statement type encountered!');
-                }
-
-                $kindMap = [
-                    Node\Stmt\Use_::TYPE_NORMAL   => UseStatementKind::TYPE_CLASSLIKE,
-                    Node\Stmt\Use_::TYPE_FUNCTION => UseStatementKind::TYPE_FUNCTION,
-                    Node\Stmt\Use_::TYPE_CONSTANT => UseStatementKind::TYPE_CONSTANT
-                ];
-
-                // NOTE: The namespace may be null here (intended behavior).
-                $this->namespaces[$this->lastNamespaceIndex]['useStatements'][(string) $use->alias] = [
-                    'name'  => $prefix . ((string) $use->name),
-                    'alias' => $use->alias,
-                    'kind'  => $kindMap[$type],
-                    'line'  => $node->getLine(),
-                    'start' => $use->getAttribute('startFilePos') ? $use->getAttribute('startFilePos')   : null,
-                    'end'   => $use->getAttribute('endFilePos')   ? $use->getAttribute('endFilePos') + 1 : null
-                ];
-            }
+            $this->registerImportNode($node);
         }
 
         if (isset($this->namespaces[$this->lastNamespaceIndex])) {
@@ -117,5 +83,69 @@ class UseStatementFetchingVisitor extends NodeVisitorAbstract
     public function getNamespaces(): array
     {
         return $this->namespaces;
+    }
+
+    /**
+     * @param Node\Stmt\Namespace_ $node
+     */
+    protected function beginNamespace(Node\Stmt\Namespace_ $node): void
+    {
+        $this->namespaces[$this->lastNamespaceIndex]['endLine'] = $node->getLine() - 1;
+        $this->namespaces[++$this->lastNamespaceIndex] = [
+            'name'          => $node->name ? (string) $node->name : null,
+            'startLine'     => $node->getLine(),
+            'endLine'       => $node->getLine() + 1,
+            'useStatements' => []
+        ];
+    }
+
+    /**
+     * @param Node\Stmt\Use_|Node\Stmt\GroupUse $node
+     *
+     * @return void
+     */
+    protected function registerImportNode(Node $node): void
+    {
+        $prefix = '';
+
+        if ($node instanceof Node\Stmt\GroupUse) {
+            $prefix = ((string) $node->prefix) . '\\';
+        };
+
+        foreach ($node->uses as $use) {
+            $this->registerImport($node, $use, $prefix);
+        }
+    }
+
+    /**
+     * @param Node\Stmt\Use_|Node\Stmt\GroupUse $node
+     * @param Node\Stmt\UseUse                  $use
+     * @param string                            $prefix
+     *
+     * @return void
+     */
+    protected function registerImport(Node $node, Node\Stmt\UseUse $use, string $prefix): void
+    {
+        $type = $node->type === Node\Stmt\Use_::TYPE_UNKNOWN ? $use->type : $node->type;
+
+        if ($type === Node\Stmt\Use_::TYPE_UNKNOWN) {
+            throw new DomainException('Unknown use statement type encountered!');
+        }
+
+        $kindMap = [
+            Node\Stmt\Use_::TYPE_NORMAL   => UseStatementKind::TYPE_CLASSLIKE,
+            Node\Stmt\Use_::TYPE_FUNCTION => UseStatementKind::TYPE_FUNCTION,
+            Node\Stmt\Use_::TYPE_CONSTANT => UseStatementKind::TYPE_CONSTANT
+        ];
+
+        // NOTE: The namespace may be null here (intended behavior).
+        $this->namespaces[$this->lastNamespaceIndex]['useStatements'][(string) $use->alias] = [
+            'name'  => $prefix . ((string) $use->name),
+            'alias' => $use->alias,
+            'kind'  => $kindMap[$type],
+            'line'  => $node->getLine(),
+            'start' => $use->getAttribute('startFilePos') ? $use->getAttribute('startFilePos')   : null,
+            'end'   => $use->getAttribute('endFilePos')   ? $use->getAttribute('endFilePos') + 1 : null
+        ];
     }
 }
