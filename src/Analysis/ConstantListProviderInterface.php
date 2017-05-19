4c800f10fbfdedc7a728963f06ec483b594a809d
Gert <Gert-dev@users.noreply.github.com>
2017-05-19 21:32:16 +0200

    Remove unused import

--- src/Analysis/ConstantListProviderInterface.php
+++ src/Analysis/ConstantListProviderInterface.php
@@ -4,8 +4,6 @@ namespace PhpIntegrator\Analysis;
 
 use PhpIntegrator\Analysis\Conversion\FunctionConverter;
 
-use PhpIntegrator\Indexing\IndexDatabase;
-
 /**
  * Retrieves a list of (global) constants.
  */
