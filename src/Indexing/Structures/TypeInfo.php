9eb97cda82788224383996e7ca3ad60082cc7c81
Gert <Gert-dev@users.noreply.github.com>
2017-05-19 19:47:26 +0200

    Remove unused property

--- src/Indexing/Structures/TypeInfo.php
+++ src/Indexing/Structures/TypeInfo.php
@@ -20,25 +20,14 @@ class TypeInfo
     private $fqcn;
 
     /**
-     * FQCN of the type after it has been resolved.
-     *
-     * For example, if the type is "static", the FQCN won't hold anything useful. The actual type must be determined
-     * based on its context, such as what class we are in, is the method inherited, and so on.
-     *
-     * @var string
-     */
-    private $resolvedType;
-
-    /**
      * @param string      $type
      * @param string      $fqcn
      * @param string|null $resolvedType
      */
-    public function __construct(string $type, string $fqcn, ?string $resolvedType = null)
+    public function __construct(string $type, string $fqcn)
     {
         $this->type = $type;
         $this->fqcn = $fqcn;
-        $this->resolvedType = $resolvedType;
     }
 
     /**
@@ -55,27 +44,5 @@ class TypeInfo
     public function getFqcn(): string
     {
         return $this->fqcn;
-    }
-
-    /**
-     * @return string
-     */
-    public function getResolvedType(): string
-    {
-        if ($this->resolvedType === null) {
-            throw new LogicException('Resolved type has not been determined yet');
-        }
-
-        return $this->resolvedType;
-    }
-
-    /**
-     * @param string $resolvedType
-     *
-     * @return void
-     */
-    public function setResolvedType(string $resolvedType): void
-    {
-        $this->resolvedType = $resolvedType;
     }
 }
