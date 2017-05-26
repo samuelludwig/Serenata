281a1b7f863184f8509a7351eec1ab49dbe31323
Gert <Gert-dev@users.noreply.github.com>
2017-05-26 20:16:57 +0200

    Add basic FunctionAutocompletionProvider test

---
+++ tests/Integration/Analysis/Autocompletion/FunctionAutocompletionProviderTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace PhpIntegrator\Tests\Integration\Analysis\Autocompletion;
+
+use PhpIntegrator\Analysis\Autocompletion\AutocompletionSuggestion;
+use PhpIntegrator\Analysis\Autocompletion\SuggestionKind;
+
+use PhpIntegrator\Tests\Integration\AbstractIntegrationTest;
+
+class FunctionAutocompletionProviderTest extends AbstractIntegrationTest
+{
+    /**
+     * @param string $file
+     *
+     * @return string[]
+     */
+    protected function provide(string $file): array
+    {
+        $path = __DIR__ . '/FunctionAutocompletionProviderTest/' . $file;
+
+        $markerOffset = $this->getMarkerOffset($path, '<MARKER>');
+
+        $container = $this->createTestContainer();
+
+        $this->indexTestFile($container, $path);
+
+        $provider = $container->get('functionAutocompletionProvider');
+
+        return $provider->provide($path, $markerOffset);
+    }
+
+    /**
+     * @param string $path
+     * @param string $marker
+     *
+     * @return int
+     */
+    protected function getMarkerOffset(string $path, string $marker): int
+    {
+        $testFileContents = @file_get_contents($path);
+
+        $markerOffset = mb_strpos($testFileContents, $marker);
+
+        return $markerOffset;
+    }
+
+    /**
+     * @return void
+     */
+    public function testRetrievesAllClasses(): void
+    {
+        $output = $this->provide('Functions.phpt');
+
+        $suggestions = [
+            new AutocompletionSuggestion('foo', SuggestionKind::FUNCTION, 'foo()', 'foo()', null, [
+                'isDeprecated'                  => false,
+                'protectionLevel'               => null,
+                'declaringStructure'            => null,
+                'url'                           => null,
+                'returnTypes'                   => '',
+                'placeCursorBetweenParentheses' => false
+            ])
+        ];
+
+        $this->assertEquals($suggestions, $output);
+    }
+}
