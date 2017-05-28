0badb0979da33db4c7462f6e293a9a4fad337855
Gert <Gert-dev@users.noreply.github.com>
2017-05-28 15:46:06 +0200

    Add performance test for a large file

--- tests/Performance/IndexingPerformanceTest.php
+++ tests/Performance/IndexingPerformanceTest.php
@@ -26,4 +26,25 @@ class IndexingPerformanceTest extends AbstractPerformanceTest
 
         unlink($dummyDatabasePath);
     }
+
+    /**
+     * @return void
+     */
+    public function testIndexLargeFile(): void
+    {
+        // This file is about 3000 lines at the time of writing.
+        $pathToIndex = __DIR__ . '/../../vendor/doctrine/orm/lib/Doctrine/ORM/UnitOfWork.php';
+        $dummyDatabasePath = $this->getOutputDirectory() . '/test-large-file.sqlite';
+
+        @unlink($dummyDatabasePath);
+
+        $this->container->get('managerRegistry')->setDatabasePath($dummyDatabasePath);
+        $this->container->get('initializeCommand')->initialize(false);
+
+        $this->indexPath($this->container, $pathToIndex);
+
+        $this->assertTrue(true);
+
+        unlink($dummyDatabasePath);
+    }
 }
