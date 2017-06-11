d2d7c5cf8b2a145ef894449f6d1c6da52c2733ff
Gert <Gert-dev@users.noreply.github.com>
2017-06-11 12:41:14 +0200

    Add additional function list performance test

--- tests/Performance/FunctionListProvidingPerformanceTest.php
+++ tests/Performance/FunctionListProvidingPerformanceTest.php
@@ -30,4 +30,29 @@ class FunctionListProvidingPerformanceTest extends AbstractPerformanceTest
 
         $this->finish($time);
     }
+
+    /**
+     * @return void
+     */
+    public function testFetchAllHotFromStubs(): void
+    {
+        $pathToIndex = __DIR__ . '/../../vendor/jetbrains/phpstorm-stubs';
+        $dummyDatabasePath = $this->getOutputDirectory() . '/test-global-functions-stubs.sqlite';
+
+        @unlink($dummyDatabasePath);
+
+        $this->container->get('managerRegistry')->setDatabasePath($dummyDatabasePath);
+        $this->container->get('initializeCommand')->initialize(false);
+
+        $this->indexPath($this->container, $pathToIndex);
+        $this->container->get('functionListProvider')->getAll();
+
+        $time = $this->time(function () use ($pathToIndex) {
+            $this->container->get('functionListProvider')->getAll();
+        });
+
+        unlink($dummyDatabasePath);
+
+        $this->finish($time);
+    }
 }
