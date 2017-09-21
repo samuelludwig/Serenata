e242b4e4be3d7db8f598c6c2a898b76f35b97a73
Gert <Gert-dev@users.noreply.github.com>
2017-09-21 21:45:36 +0200

    Stub remaining test methods
    References #42

--- tests/Integration/GotoDefinition/DefinitionLocatorTest.php
+++ tests/Integration/GotoDefinition/DefinitionLocatorTest.php
@@ -28,348 +28,153 @@ class DefinitionLocatorTest extends AbstractIntegrationTest
     // /**
     //  * @return void
     //  */
-    // public function testFunctionCallWhitespaceBeforeFirstAndOnlyArgument(): void
-    // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('test', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.')
-    //         ])
-    //     ];
-    //
-    //     $fileName = 'FunctionCallWhitespaceBeforeFirstAndOnlyArgument.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 108, 110, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 108, 110, 0);
-    // }
-    //
-    // /**
-    //  * @return void
-    //  */
-    // public function testFunctionCallWhitespaceBetweenArguments(): void
-    // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('test', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.'),
-    //             new ParameterInformation('int $b', 'Parameter B.')
-    //         ])
-    //     ];
-    //
-    //     $fileName = 'FunctionCallWhitespaceBetweenArguments.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 142, 147, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 142, 144, 0);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 145, 147, 1);
-    // }
-    //
-    // /**
-    //  * @return void
-    //  */
-    // public function testFunctionCallWithMissingLastArgumentAfterComma(): void
-    // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('test', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.'),
-    //             new ParameterInformation('int $b', 'Parameter B.')
-    //         ])
-    //     ];
-    //
-    //     $fileName = 'FunctionCallWithMissingLastArgumentAfterComma.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 142, 144, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 142, 143, 0);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 144, 144, 1);
-    // }
-    //
-    // // /**
-    // //  * @return void
-    // //  */
-    // // public function testFunctionCallDoesNotWorkWhenInsideClosureArgumentBody(): void
-    // // {
-    // //     $expectedSignaturesResult = [
-    // //         new SignatureInformation('test', null, [
-    // //             new ParameterInformation('\Closure $a', null)
-    // //         ])
-    // //     ];
-    // //
-    // //     $fileName = 'FunctionCallClosureArgumentBody.phpt';
-    // //
-    // //     $this->assertSignatureHelpSignaturesEquals($fileName, 80, 94, $expectedSignaturesResult);
-    // //
-    // //     $hadException = false;
-    // //
-    // //     try {
-    // //         $this->assertSignatureHelpSignaturesEquals($fileName, 95, 95, $expectedSignaturesResult);
-    // //     } catch (UnexpectedValueException $e) {
-    // //         $hadException = true;
-    // //     }
-    // //
-    // //     $this->assertTrue($hadException, 'Signature help should not trigger inside the body of closure arguments');
-    // //
-    // //     $hadException = false;
-    // //
-    // //     try {
-    // //         $this->assertSignatureHelpSignaturesEquals($fileName, 101, 101, $expectedSignaturesResult);
-    // //     } catch (UnexpectedValueException $e) {
-    // //         $hadException = true;
-    // //     }
-    // //
-    // //     $this->assertTrue($hadException, 'Signature help should not trigger inside the body of closure arguments');
-    // //
-    // //     $this->assertSignatureHelpSignaturesEquals($fileName, 102, 102, $expectedSignaturesResult);
-    // // }
-    //
-    // /**
-    //  * @return void
-    //  */
-    // public function testNestedFunctionCall(): void
-    // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('foo', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.')
-    //         ])
-    //     ];
-    //
-    //     $expectedNestedSignaturesResult = [
-    //         new SignatureInformation('bar', null, [])
-    //     ];
-    //
-    //     $fileName = 'NestedFunctionCall.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 127, 131, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 133, 134, $expectedNestedSignaturesResult);
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 135, 136, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 127, 136, 0);
-    // }
-    //
-    // /**
-    //  * @return void
-    //  */
     // public function testMethodCall(): void
     // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('test', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.'),
-    //             new ParameterInformation('bool $b = true', null),
-    //             new ParameterInformation('string $c', 'Parameter C.')
-    //         ])
-    //     ];
-    //
-    //     $fileName = 'MethodCall.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 245, 252, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 245, 246, 0);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 247, 249, 1);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 250, 252, 2);
+    //     // TODO
     // }
     //
     // /**
     //  * @return void
     //  */
-    // public function testNestedMethodCall(): void
+    // public function testConstant(): void
     // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('foo', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.')
-    //         ])
-    //     ];
-    //
-    //     $expectedNestedSignaturesResult = [
-    //         new SignatureInformation('bar', null, [])
-    //     ];
-    //
-    //     $fileName = 'NestedMethodCall.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 221, 232, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 233, 235, $expectedNestedSignaturesResult);
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 236, 237, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 221, 237, 0);
+    //     // TODO
     // }
     //
     // /**
     //  * @return void
     //  */
-    // public function testStaticMethodCall(): void
+    // public function testClassConstantConstant(): void
     // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('test', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.'),
-    //             new ParameterInformation('bool $b = true', null),
-    //             new ParameterInformation('string $c', 'Parameter C.')
-    //         ])
-    //     ];
-    //
-    //     $fileName = 'StaticMethodCall.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 253, 260, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 253, 254, 0);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 255, 257, 1);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 258, 260, 2);
+    //     // TODO
     // }
     //
     // /**
     //  * @return void
     //  */
-    // public function testConstructor(): void
+    // public function testClassConstantClass(): void
     // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('__construct', 'Some summary.', [
-    //             new ParameterInformation('int $a', 'Parameter A.'),
-    //             new ParameterInformation('bool $b = true', null),
-    //             new ParameterInformation('string $c', 'Parameter C.')
-    //         ])
-    //     ];
-    //
-    //     $fileName = 'Constructor.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 256, 309, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 256, 270, 0);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 271, 285, 1);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 286, 309, 2);
+    //     // TODO
     // }
     //
     // /**
     //  * @return void
     //  */
-    // public function testNestedConstructor(): void
+    // public function testStaticMethodCallMethod(): void
     // {
-    //     $expectedSignaturesResult = [
-    //         new SignatureInformation('__construct', null, [
-    //             new ParameterInformation('int $a', null)
-    //         ])
-    //     ];
-    //
-    //     $expectedNestedSignaturesResult = [
-    //         new SignatureInformation('__construct', null, [
-    //             new ParameterInformation('int $b', null)
-    //         ])
-    //     ];
-    //
-    //     $fileName = 'NestedConstructor.phpt';
-    //
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 219, 227, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 228, 231, $expectedNestedSignaturesResult);
-    //     $this->assertSignatureHelpSignaturesEquals($fileName, 232, 233, $expectedSignaturesResult);
-    //     $this->assertSignatureHelpActiveParameterEquals($fileName, 219, 233, 0);
+    //     // TODO
     // }
     //
     // /**
     //  * @return void
     //  */
-    // public function testArgumentIndexIsNullForFunctionCallFunctionWithoutArguments(): void
+    // public function testStaticMethodCallClass(): void
     // {
-    //     $result = $this->getSignatureHelp('FunctionCallFunctionWithoutArguments.phpt', 48);
-    //
-    //     $this->assertCount(1, $result->getSignatures());
-    //     $this->assertEmpty($result->getSignatures()[0]->getParameters());
-    //     $this->assertNull($result->getActiveParameter());
+    //     // TODO
     // }
     //
     // /**
     //  * @return void
     //  */
-    // public function testArgumentIndexIsCorrectWithVariadicParameters(): void
+    // public function testPropertyFetch(): void
     // {
-    //     $result = $this->getSignatureHelp('VariadicParameter.phpt', 217);
-    //
-    //     $this->assertCount(1, $result->getSignatures());
-    //     $this->assertCount(2, $result->getSignatures()[0]->getParameters());
-    //     $this->assertEquals(1, $result->getActiveParameter());
-    //
-    //     $result = $this->getSignatureHelp('VariadicParameter.phpt', 220);
-    //
-    //     $this->assertCount(1, $result->getSignatures());
-    //     $this->assertCount(2, $result->getSignatures()[0]->getParameters());
-    //     $this->assertEquals(1, $result->getActiveParameter());
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testFunctionCallFunctionNameDoesNotWorkWhenFunctionDoesNotHaveArguments(): void
+    // public function testStaticPropertyFetchProperty(): void
     // {
-    //     $result = $this->getSignatureHelp('FunctionCallFunctionNameWithoutArguments.phpt', 47);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testFunctionCallFunctionNameDoesNotWorkWhenFunctionHasArguments(): void
+    // public function testStaticPropertyFetchClass(): void
     // {
-    //     $result = $this->getSignatureHelp('FunctionCallFunctionNameWithArguments.phpt', 53);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testFunctionCallFailsWhenArgumentIsOutOfBounds(): void
+    // public function testUseStatementClass(): void
     // {
-    //     $result = $this->getSignatureHelp('FunctionCallTooManyArguments.phpt', 113);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testFunctionCallFailsWhenTrailingCommaIsPresentThatWouldBeFollowedByOutOfBoundsArgument(): void
+    // public function testGroupedUseStatementClass(): void
     // {
-    //     $result = $this->getSignatureHelp('TrailingCommaIsPresentThatWouldBeFollowedByOutOfBoundsArgument.phpt', 97);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testDynamicFunctionNameFails(): void
+    // public function testImplementsClass(): void
     // {
-    //     $result = $this->getSignatureHelp('FunctionCallDynamic.phpt', 49);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testDynamicMethodNameFails(): void
+    // public function testExtendsClass(): void
     // {
-    //     $result = $this->getSignatureHelp('MethodCallDynamic.phpt', 85);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testDynamicConstructorCallFails(): void
+    // public function testTraitUseClass(): void
     // {
-    //     $result = $this->getSignatureHelp('ConstructorDynamic.phpt', 111);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testNoInvocationFails(): void
+    // public function testTraitUseInsteadofClass(): void
     // {
-    //     $result = $this->getSignatureHelp('NoInvocation.phpt', 233);
+    //     // TODO
     // }
     //
     // /**
-    //  * @expectedException \UnexpectedValueException
-    //  *
     //  * @return void
     //  */
-    // public function testNoInvocationWithMissingMemberNameFails(): void
+    // public function testTraitUseAliasClass(): void
     // {
-    //     $result = $this->getSignatureHelp('NoInvocationMissingMember.phpt', 17);
+    //     // TODO
+    // }
+    //
+    // /**
+    //  * @return void
+    //  */
+    // public function testCommentClassAfterVarTag(): void
+    // {
+    //     // TODO
+    // }
+    //
+    // /**
+    //  * @return void
+    //  */
+    // public function testCommentClassAfterParamTag(): void
+    // {
+    //     // TODO
+    // }
+    //
+    // /**
+    //  * @return void
+    //  */
+    // public function testCommentClassAfterReturnTag(): void
+    // {
+    //     // TODO
     // }
 
     /**
