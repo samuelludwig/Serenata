a6564748e7c79af53c90649d1faf5e5ceb602aa1
Gert <Gert-dev@users.noreply.github.com>
2017-10-24 19:21:17 +0200

    Fix incorrect indentation

--- src/Analysis/ClasslikeListRegistry.php
+++ src/Analysis/ClasslikeListRegistry.php
@@ -25,67 +25,67 @@ final class ClasslikeListRegistry implements ClasslikeListProviderInterface
         $this->delegate = $delegate;
     }
 
-     /// @inherited
-     public function getAll(): array
-     {
-         return $this->getRegistry();
-     }
+    /// @inherited
+    public function getAll(): array
+    {
+        return $this->getRegistry();
+    }
 
-     /**
-      * @param array $classlike
-      */
-     public function add(array $classlike): void
-     {
-         $this->initializeRegistryIfNecessary();
+    /**
+     * @param array $classlike
+     */
+    public function add(array $classlike): void
+    {
+        $this->initializeRegistryIfNecessary();
 
-         $this->registry[$classlike['fqcn']] = $classlike;
-     }
+        $this->registry[$classlike['fqcn']] = $classlike;
+    }
 
-     /**
-      * @param array $classlike
-      */
-     public function remove(array $classlike): void
-     {
-         $this->initializeRegistryIfNecessary();
+    /**
+     * @param array $classlike
+     */
+    public function remove(array $classlike): void
+    {
+        $this->initializeRegistryIfNecessary();
 
-         if (isset($this->registry[$classlike['fqcn']])) {
-             unset($this->registry[$classlike['fqcn']]);
-         }
-     }
+        if (isset($this->registry[$classlike['fqcn']])) {
+            unset($this->registry[$classlike['fqcn']]);
+        }
+    }
 
-     /**
-      * @return void
-      */
-     public function reset(): void
-     {
-         $this->registry = null;
-     }
+    /**
+     * @return void
+     */
+    public function reset(): void
+    {
+        $this->registry = null;
+    }
 
-     /**
-      * @return array
-      */
-     protected function getRegistry(): array
-     {
-         $this->initializeRegistryIfNecessary();
+    /**
+     * @return array
+     */
+    protected function getRegistry(): array
+    {
+        $this->initializeRegistryIfNecessary();
 
-         return $this->registry;
-     }
+        return $this->registry;
+    }
 
-     /**
-      * @return void
-      */
-     protected function initializeRegistryIfNecessary(): void
-     {
-         if ($this->registry === null) {
-             $this->initializeRegistry();
-         }
-     }
+    /**
+     * @return void
+     */
+    protected function initializeRegistryIfNecessary(): void
+    {
+        if ($this->registry === null) {
+            $this->initializeRegistry();
+        }
+    }
 
-     /**
-      * @return void
-      */
-     protected function initializeRegistry(): void
-     {
-         $this->registry = $this->delegate->getAll();
-     }
+    /**
+     * @return void
+     */
+    protected function initializeRegistry(): void
+    {
+        $this->registry = $this->delegate->getAll();
+    }
 }
