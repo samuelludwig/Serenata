c71a0b27caaab9c0c5580f03dd998f10da137caa
Gert <Gert-dev@users.noreply.github.com>
2017-10-25 20:03:07 +0200

    Fix incorrect indentation

--- src/Analysis/NamespaceListProviderInterface.php
+++ src/Analysis/NamespaceListProviderInterface.php
@@ -9,10 +9,10 @@ use RuntimeException;
  */
 interface NamespaceListProviderInterface
 {
-     /**
-      * @throws RuntimeException
-      *
-      * @return array[]
-      */
-     public function getAll(): array;
+    /**
+     * @throws RuntimeException
+     *
+     * @return array[]
+     */
+    public function getAll(): array;
 }
