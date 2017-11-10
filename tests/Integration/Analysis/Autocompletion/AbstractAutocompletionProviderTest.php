8fb67cceca06dfe62e91508c878eb6d7b5d213c2
Gert <Gert-dev@users.noreply.github.com>
2017-11-10 20:54:15 +0100

    Extract test base class
    References #42

---
+++ tests/Integration/Analysis/Autocompletion/AbstractAutocompletionProviderTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PhpIntegrator\Tests\Integration\Analysis\Autocompletion;
+
+use PhpIntegrator\Tests\Integration\AbstractIntegrationTest;
+
+/**
+ * Abstract base class for autocompletion provider integration tests.
+ */
+abstract class AbstractAutocompletionProviderTest extends AbstractIntegrationTest
+{
+    /**
+     * @param string $file
+     *
+     * @return string[]
+     */
+    protected function provide(string $file): array
+    {
+        $path = __DIR__ . '/FunctionAutocompletionProviderTest/' . $file;
+
+        $markerString = '// <MARKER>';
+
+        $markerOffset = $this->getMarkerOffset($path, $markerString);
+
+        $container = $this->createTestContainer();
+
+        // Strip marker so it does not influence further processing.
+        $code = $container->get('sourceCodeStreamReader')->getSourceCodeFromFile($path);
+        $code = str_replace($markerString, '', $code);
+
+        $this->indexTestFileWithSource($container, $path, $code);
+
+        $provider = $container->get('autocompletionProvider');
+
+        return iterator_to_array($provider->provide($code, $markerOffset), false);
+    }
+
+    /**
+     * @param string $path
+     * @param string $marker
+     *
+     * @return int
+     */
+    protected function getMarkerOffset(string $path, string $marker): int
+    {
+        $testFileContents = @file_get_contents($path);
+
+        $markerOffset = mb_strpos($testFileContents, $marker);
+
+        return $markerOffset;
+    }
+}
--- tests/Integration/Analysis/Autocompletion/FunctionAutocompletionProviderTest.php
+++ tests/Integration/Analysis/Autocompletion/FunctionAutocompletionProviderTest.php
@@ -2,54 +2,11 @@
 
 namespace PhpIntegrator\Tests\Integration\Analysis\Autocompletion;
 
-use PhpIntegrator\Analysis\Autocompletion\AutocompletionSuggestion;
 use PhpIntegrator\Analysis\Autocompletion\SuggestionKind;
+use PhpIntegrator\Analysis\Autocompletion\AutocompletionSuggestion;
 
-use PhpIntegrator\Tests\Integration\AbstractIntegrationTest;
-
-class FunctionAutocompletionProviderTest extends AbstractIntegrationTest
+class FunctionAutocompletionProviderTest extends AbstractAutocompletionProviderTest
 {
-    /**
-     * @param string $file
-     *
-     * @return string[]
-     */
-    protected function provide(string $file): array
-    {
-        $path = __DIR__ . '/FunctionAutocompletionProviderTest/' . $file;
-
-        $markerString = '// <MARKER>';
-
-        $markerOffset = $this->getMarkerOffset($path, $markerString);
-
-        $container = $this->createTestContainer();
-
-        // Strip marker so it does not influence further processing.
-        $code = $container->get('sourceCodeStreamReader')->getSourceCodeFromFile($path);
-        $code = str_replace($markerString, '', $code);
-
-        $this->indexTestFileWithSource($container, $path, $code);
-
-        $provider = $container->get('functionAutocompletionProvider');
-
-        return iterator_to_array($provider->provide($code, $markerOffset), false);
-    }
-
-    /**
-     * @param string $path
-     * @param string $marker
-     *
-     * @return int
-     */
-    protected function getMarkerOffset(string $path, string $marker): int
-    {
-        $testFileContents = @file_get_contents($path);
-
-        $markerOffset = mb_strpos($testFileContents, $marker);
-
-        return $markerOffset;
-    }
-
     /**
      * @return void
      */
