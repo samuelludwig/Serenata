6f95c52eb8111a0703be4261255c2a1aec16e0ae
Gert <Gert-dev@users.noreply.github.com>
2017-11-11 11:32:35 +0100

    Update comment

--- src/Main.php
+++ src/Main.php
@@ -22,7 +22,8 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set('xdebug.max_nesting_level', 10000);
 
 // xdebug will only slow down indexing. Very strangely enough, disabling xdebug doesn't seem to disable this nesting
-// level in all cases. See also https://github.com/Gert-dev/php-integrator-base/issues/101 .
+// level in all cases. See also https://github.com/Gert-dev/php-integrator-base/issues/101 . This appears to be
+// confirmed in https://github.com/nikic/PHP-Parser/blob/master/doc/component/Performance.markdown
 if (function_exists('xdebug_disable')) {
     xdebug_disable();
 }
