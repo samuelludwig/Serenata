3850acf24dc8c4316b22ffdcc2c914a4aaa9fba4
Gert <Gert-dev@users.noreply.github.com>
2017-12-03 18:19:26 +0100

    Add additional function autocompletion tests
    References #43

--- tests/Integration/Analysis/Autocompletion/FunctionAutocompletionProviderTest.php
+++ tests/Integration/Analysis/Autocompletion/FunctionAutocompletionProviderTest.php
@@ -71,6 +71,48 @@ class FunctionAutocompletionProviderTest extends AbstractAutocompletionProviderT
     }
 
     /**
+     * @return void
+     */
+    public function testSuggestsPlacingCursorBetweenParanthesesWhenParametersExist(): void
+    {
+        $output = $this->provide('FunctionWithParameters.phpt');
+
+        $suggestions = [
+            new AutocompletionSuggestion('foo', SuggestionKind::FUNCTION, 'foo()', 'foo($test)', null, [
+                'isDeprecated'                  => false,
+                'protectionLevel'               => null,
+                'declaringStructure'            => null,
+                'url'                           => null,
+                'returnTypes'                   => '',
+                'placeCursorBetweenParentheses' => true
+            ])
+        ];
+
+        static::assertEquals($suggestions, $output);
+    }
+
+    /**
+     * @return void
+     */
+    public function testMarksDeprecatedFunctionAsDeprecated(): void
+    {
+        $output = $this->provide('DeprecatedFunction.phpt');
+
+        $suggestions = [
+            new AutocompletionSuggestion('foo', SuggestionKind::FUNCTION, 'foo()', 'foo()', null, [
+                'isDeprecated'                  => true,
+                'protectionLevel'               => null,
+                'declaringStructure'            => null,
+                'url'                           => null,
+                'returnTypes'                   => 'void',
+                'placeCursorBetweenParentheses' => false
+            ])
+        ];
+
+        static::assertEquals($suggestions, $output);
+    }
+
+    /**
      * @inheritDoc
      */
     protected function getFolderName(): string
