98251084abe519fe15c78ad255851e3f3045e290
Gert <Gert-dev@users.noreply.github.com>
2017-12-16 22:34:17 +0100

    Implement class constant autocompletion provider
    References #43

---
+++ src/Analysis/Autocompletion/ClassConstantAutocompletionProvider.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace PhpIntegrator\Analysis\Autocompletion;
+
+use Generator;
+use AssertionError;
+use UnexpectedValueException;
+
+use PhpIntegrator\Analysis\ClasslikeInfoBuilder;
+use PhpIntegrator\Analysis\CircularDependencyException;
+
+use PhpIntegrator\Analysis\Typing\Deduction\ExpressionTypeDeducer;
+
+use PhpIntegrator\Indexing\Structures\File;
+
+/**
+ * Provides member constant autocompletion suggestions at a specific location in a file.
+ */
+final class ClassConstantAutocompletionProvider implements AutocompletionProviderInterface
+{
+    /**
+     * @var ExpressionTypeDeducer
+     */
+    private $expressionTypeDeducer;
+
+    /**
+     * @var ClasslikeInfoBuilder
+     */
+    private $classlikeInfoBuilder;
+
+    /**
+     * @param ExpressionTypeDeducer $expressionTypeDeducer
+     * @param ClasslikeInfoBuilder  $classlikeInfoBuilder
+     */
+    public function __construct(
+        ExpressionTypeDeducer $expressionTypeDeducer,
+        ClasslikeInfoBuilder $classlikeInfoBuilder
+    ) {
+        $this->expressionTypeDeducer = $expressionTypeDeducer;
+        $this->classlikeInfoBuilder = $classlikeInfoBuilder;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function provide(File $file, string $code, int $offset): iterable
+    {
+        $types = $this->expressionTypeDeducer->deduce(
+            $file,
+            $code,
+            $offset,
+            null,
+            true
+        );
+
+        $classlikeInfoElements = array_map(function (string $type) {
+            try {
+                return $this->classlikeInfoBuilder->getClasslikeInfo($type);
+            } catch (UnexpectedValueException|CircularDependencyException $e) {
+                return null;
+            }
+        }, $types);
+
+        $classlikeInfoElements = array_filter($classlikeInfoElements);
+
+        foreach ($classlikeInfoElements as $classlikeInfoElement) {
+            yield from $this->createSuggestionsForClasslikeInfo($classlikeInfoElement);
+        }
+    }
+
+    /**
+     * @param array $classlikeInfo
+     *
+     * @return Generator
+     */
+    private function createSuggestionsForClasslikeInfo(array $classlikeInfo): Generator
+    {
+        foreach ($classlikeInfo['constants'] as $constant) {
+            yield $this->createSuggestion($constant);
+        }
+    }
+
+    /**
+     * @param array $constant
+     *
+     * @return AutocompletionSuggestion
+     */
+    private function createSuggestion(array $constant): AutocompletionSuggestion
+    {
+        return new AutocompletionSuggestion(
+            $constant['name'],
+            SuggestionKind::CONSTANT,
+            $constant['name'],
+            $constant['name'],
+            $constant['shortDescription'],
+            [
+                'isDeprecated'       => $constant['isDeprecated'],
+                'declaringStructure' => $constant['declaringStructure'],
+                'returnTypes'        => $this->createReturnTypes($constant),
+                'protectionLevel'    => $this->extractProtectionLevelStringFromMemberData($constant)
+            ]
+        );
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return string
+     */
+    private function extractProtectionLevelStringFromMemberData(array $data): string
+    {
+        if ($data['isPublic']) {
+            return 'public';
+        } elseif ($data['isProtected']) {
+            return 'protected';
+        } elseif ($data['isPrivate']) {
+            return 'private';
+        }
+
+        throw new AssertionError('Unknown protection level encountered');
+    }
+
+    /**
+     * @param array $function
+     *
+     * @return string
+     */
+    private function createReturnTypes(array $function): string
+    {
+        $typeNames = $this->getShortReturnTypes($function);
+
+        return implode('|', $typeNames);
+    }
+
+    /**
+     * @param array $function
+     *
+     * @return string[]
+     */
+    private function getShortReturnTypes(array $function): array
+    {
+        $shortTypes = [];
+
+        foreach ($function['types'] as $type) {
+            $shortTypes[] = $this->getClassShortNameFromFqcn($type['fqcn']);
+        }
+
+        return $shortTypes;
+    }
+
+    /**
+     * @param string $fqcn
+     *
+     * @return string
+     */
+    private function getClassShortNameFromFqcn(string $fqcn): string
+    {
+        $parts = explode('\\', $fqcn);
+
+        return array_pop($parts);
+    }
+}
---
+++ tests/Integration/Analysis/Autocompletion/ApplicabilityCheckingAutocompletionProviderTest/ClassConstantList.phpt
@@ -0,0 +1,6 @@
+<?php
+
+class A
+{
+    public const FOO = 3;
+}
