bb7af872c37a9d15f908a0cfd9966788619a36e0
Gert <Gert-dev@users.noreply.github.com>
2017-12-26 23:21:24 +0100

    Refactor ClasslikeAutocompletionProvider
    References #43

--- src/Analysis/Autocompletion/ClasslikeAutocompletionProvider.php
+++ src/Analysis/Autocompletion/ClasslikeAutocompletionProvider.php
@@ -2,6 +2,8 @@
 
 namespace PhpIntegrator\Analysis\Autocompletion;
 
+use AssertionError;
+
 use PhpIntegrator\Analysis\ClasslikeListProviderInterface;
 
 use PhpIntegrator\Analysis\Visiting\UseStatementKind;
@@ -11,6 +13,8 @@ use PhpIntegrator\Indexing\Structures\ClasslikeTypeNameValue;
 
 use PhpIntegrator\Refactoring\UseStatementInsertionCreator;
 use PhpIntegrator\Refactoring\UseStatementInsertionCreationException;
+
+use PhpIntegrator\Utility\TextEdit;
 
 /**
  * Provides classlike autocompletion suggestions at a specific location in a file.
@@ -66,101 +70,95 @@ final class ClasslikeAutocompletionProvider implements AutocompletionProviderInt
      */
     private function createSuggestion(array $classlike, string $code, int $offset): AutocompletionSuggestion
     {
-        $fqcnWithoutLeadingSlash = $classlike['fqcn'];
+        return new AutocompletionSuggestion(
+            $this->getFqcnWithoutLeadingSlash($classlike),
+            $classlike['type'] === ClasslikeTypeNameValue::TRAIT_ ? SuggestionKind::MIXIN : SuggestionKind::CLASS_,
+            $this->getInsertTextForSuggestion($classlike, $code, $offset),
+            $this->getFqcnWithoutLeadingSlash($classlike),
+            $classlike['shortDescription'],
+            [
+                'isDeprecated' => $classlike['isDeprecated'],
+                'returnTypes'  => $classlike['type']
+            ],
+            $this->createAdditionalTextEditsForSuggestion($classlike, $code, $offset)
+        );
+    }
 
-        if ($fqcnWithoutLeadingSlash[0] === '\\') {
-            $fqcnWithoutLeadingSlash = mb_substr($fqcnWithoutLeadingSlash, 1);
+    /**
+     * @param array $classlike
+     *
+     * @return string
+     */
+    private function getFqcnWithoutLeadingSlash(array $classlike): string
+    {
+        $fqcn = $classlike['fqcn'];
+
+        if ($fqcn[0] === '\\') {
+            return mb_substr($fqcn, 1);
         }
 
-        $additionalTextEdits = [];
-        $insertText = $fqcnWithoutLeadingSlash;
+        return $fqcn;
+    }
 
-        $prefixHasLeadingSlash = false;
+    /**
+     * @param array  $classlike
+     * @param string $code
+     * @param int    $offset
+     *
+     * @return string
+     */
+    private function getInsertTextForSuggestion(array $classlike, string $code, int $offset): string
+    {
         $prefix = $this->autocompletionPrefixDeterminer->determine($code, $offset);
 
-        if ($prefix !== '' && $prefix[0] == '\\') {
-            $prefixHasLeadingSlash = true;
-            $prefix = mb_substr($prefix, 1);
+        if ($prefix !== '' && $prefix[0] === '\\') {
+            return $classlike['fqcn'];
         }
-
-        $prefixParts = explode('\\', $prefix);
-        $partsToSlice = (count($prefixParts) - 1);
 
         // We try to add an import that has only as many parts of the namespace as needed, for example, if the user
         // types 'Foo\Class' and confirms the suggestion 'My\Foo\Class', we add an import for 'My\Foo' and leave the
         // user's code at 'Foo\Class' as a relative import. We only add the full 'My\Foo\Class' if the user were to
         // type just 'Class' and then select 'My\Foo\Class' (i.e. we remove as many segments from the suggestion
         // as the user already has in his code).
-        if ($prefixHasLeadingSlash) {
-            $insertText = '\\' . $fqcnWithoutLeadingSlash;
-        } else {
-            // Don't try to add use statements for class names that the user wants to make absolute by adding a leading
-            // slash.
-            $insertText = $this->getNameToInsert($fqcnWithoutLeadingSlash, $partsToSlice);
-            $nameToImport = $this->getNameToImportViaUseStatement($fqcnWithoutLeadingSlash, $partsToSlice);
+        $partsToSlice = (count(explode('\\', $prefix)) - 1);
+        $parts = explode('\\', $this->getFqcnWithoutLeadingSlash($classlike));
 
-            try {
-                $additionalTextEdits[] = $this->useStatementInsertionCreator->create(
-                    $nameToImport,
-                    UseStatementKind::TYPE_CLASSLIKE,
-                    $code,
-                    $offset,
-                    true
-                );
-            } catch (UseStatementInsertionCreationException $e) {
-                $additionalTextEdits = [];
-            }
-        }
-
-        return new AutocompletionSuggestion(
-            $fqcnWithoutLeadingSlash,
-            $classlike['type'] === ClasslikeTypeNameValue::TRAIT_ ? SuggestionKind::MIXIN : SuggestionKind::CLASS_,
-            $insertText,
-            $fqcnWithoutLeadingSlash,
-            $classlike['shortDescription'],
-            [
-                'isDeprecated' => $classlike['isDeprecated'],
-                'returnTypes'  => $classlike['type']
-            ],
-            $additionalTextEdits
-        );
+        // Don't try to add use statements for class names that the user wants to make absolute by adding a leading
+        // slash.
+        return implode('\\', array_slice($parts, -$partsToSlice - 1));
     }
 
     /**
-     * Returns the name to insert into the buffer.
+     * @param array  $classlike
+     * @param string $code
+     * @param int    $offset
      *
-     * @param string $name                 The FQCN of the class that needs to be imported.
-     * @param int    $extraPartsToMaintain The amount of parts to leave extra for the class name. For example, a value
-     *                                     of 1 will return B\C instead of A\B\C. A value of 0 will return just C.
-     *
-     * @return string|null
+     * @return TextEdit[]
      */
-    private function getNameToInsert(string $name, int $extraPartsToMaintain): ?string
+    private function createAdditionalTextEditsForSuggestion(array $classlike, string $code, int $offset): array
     {
-        if ($name[0] === '\\') {
-            $name = mb_substr($name, 1);
+        $prefix = $this->autocompletionPrefixDeterminer->determine($code, $offset);
+
+        if ($prefix !== '' && $prefix[0] === '\\') {
+            return [];
         }
 
-        return implode('\\', array_slice(explode('\\', $name), -$extraPartsToMaintain - 1));
-    }
+        $partsToSlice = (count(explode('\\', $prefix)) - 1);
+        $parts = explode('\\', $this->getFqcnWithoutLeadingSlash($classlike));
+        $nameToImport = implode('\\', array_slice($parts, 0, count($parts) - $partsToSlice));
 
-    /**
-     * Returns the name to import via a use statement.
-     *
-     * @param string $name          The FQCN of the class that needs to be imported.
-     * @param int    $partsToPopOff The amount of parts to leave off of the end of the class name. For example, a
-     *                              value of 1 will return A\B instead of A\B\C.
-     *
-     * @return string|null
-     */
-    private function getNameToImportViaUseStatement(string $name, int $partsToPopOff): ?string
-    {
-        if ($name[0] === '\\') {
-            $name = mb_substr($name, 1);
+        try {
+            return [$this->useStatementInsertionCreator->create(
+                $nameToImport,
+                UseStatementKind::TYPE_CLASSLIKE,
+                $code,
+                $offset,
+                true
+            )];
+        } catch (UseStatementInsertionCreationException $e) {
+            return [];
         }
 
-        $nameParts = explode('\\', $name);
-
-        return implode('\\', array_slice($nameParts, 0, count($nameParts) - $partsToPopOff));
+        throw new AssertionError('Should never be reached');
     }
 }
