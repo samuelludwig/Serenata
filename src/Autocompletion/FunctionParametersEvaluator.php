a9ccc28def01649a1fce8e15a05ef15645cca52f
Gert <Gert-dev@users.noreply.github.com>
2018-06-08 17:30:12 +0200

    Add docblocks

--- src/Autocompletion/FunctionParametersEvaluator.php
+++ src/Autocompletion/FunctionParametersEvaluator.php
@@ -2,8 +2,16 @@
 
 namespace Serenata\Autocompletion;
 
+/**
+ * Evaluates function parameters and allows additional queries on them.
+ */
 class FunctionParametersEvaluator
 {
+    /**
+     * @param array $function
+     *
+     * @return bool
+     */
     public function hasRequiredParameters(array $function): bool
     {
         // foreach can handle empty arrays.
