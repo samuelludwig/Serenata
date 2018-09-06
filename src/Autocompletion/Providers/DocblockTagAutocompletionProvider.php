c335f802ffb4beef998555991ff07495100f10f9
Gert <Gert-dev@users.noreply.github.com>
2018-09-06 21:56:45 +0200

    Fix PSR-2 warning by reformatting array

--- src/Autocompletion/Providers/DocblockTagAutocompletionProvider.php
+++ src/Autocompletion/Providers/DocblockTagAutocompletionProvider.php
@@ -108,32 +108,135 @@ final class DocblockTagAutocompletionProvider implements AutocompletionProviderI
     private function getTags(): array
     {
         return [
-            ['name' => '@api',            'insertText' => '@api$0'],
-            ['name' => '@author',         'insertText' => '@author ${1:name} ${2:[email]}$0'],
-            ['name' => '@copyright',      'insertText' => '@copyright ${1:description}$0'],
-            ['name' => '@deprecated',     'insertText' => '@deprecated ${1:[vector]} ${2:[description]}$0'],
-            ['name' => '@example',        'insertText' => '@example ${1:example}$0'],
-            ['name' => '@filesource',     'insertText' => '@filesource$0'],
-            ['name' => '@ignore',         'insertText' => '@ignore ${1:[description]}$0'],
-            ['name' => '@inheritDoc',     'insertText' => '@inheritDoc$0'],
-            ['name' => '@internal',       'insertText' => '@internal ${1:description}$0'],
-            ['name' => '@license',        'insertText' => '@license ${1:[url]} ${2:name}$0'],
-            ['name' => '@link',           'insertText' => '@link ${1:uri} ${2:[description]}$0'],
-            ['name' => '@method',         'insertText' => '@method ${1:type} ${2:name}(${3:[parameter list]})$0'],
-            ['name' => '@package',        'insertText' => '@package ${1:package name}$0'],
-            ['name' => '@param',          'insertText' => '@param ${1:mixed} \$${2:parameter} ${3:[description]}$0'],
-            ['name' => '@property',       'insertText' => '@property ${1:type} ${2:name} ${3:[description]}$0'],
-            ['name' => '@property-read',  'insertText' => '@property-read ${1:type} ${2:name} ${3:[description]}$0'],
-            ['name' => '@property-write', 'insertText' => '@property-write ${1:type} ${2:name} ${3:[description]}$0'],
-            ['name' => '@return',         'insertText' => '@return ${1:type} ${2:[description]}$0'],
-            ['name' => '@see',            'insertText' => '@see ${1:URI or FQSEN} ${2:description}$0'],
-            ['name' => '@since',          'insertText' => '@since ${1:version} ${2:[description]}$0'],
-            ['name' => '@source',         'insertText' => '@source ${1:start line} ${2:number of lines} ${3:[description]}$0'],
-            ['name' => '@throws',         'insertText' => '@throws ${1:exception type} ${2:[description]}$0'],
-            ['name' => '@todo',           'insertText' => '@todo ${1:description}$0'],
-            ['name' => '@uses',           'insertText' => '@uses ${1:FQSEN} ${2:[description]}$0'],
-            ['name' => '@var',            'insertText' => '@var ${1:type} ${2:\$${3:[property]} ${4:[description]}}$0'],
-            ['name' => '@version',        'insertText' => '@version ${1:vector} ${2:[description]}$0'],
+            [
+                'name'       => '@api',
+                'insertText' => '@api$0',
+            ],
+
+            [
+                'name'       => '@author',
+                'insertText' => '@author ${1:name} ${2:[email]}$0',
+            ],
+
+            [
+                'name'       => '@copyright',
+                'insertText' => '@copyright ${1:description}$0',
+            ],
+
+            [
+                'name'       => '@deprecated',
+                'insertText' => '@deprecated ${1:[vector]} ${2:[description]}$0',
+            ],
+
+            [
+                'name'       => '@example',
+                'insertText' => '@example ${1:example}$0',
+            ],
+
+            [
+                'name'       => '@filesource',
+                'insertText' => '@filesource$0',
+            ],
+
+            [
+                'name'       => '@ignore',
+                'insertText' => '@ignore ${1:[description]}$0',
+            ],
+
+            [
+                'name'       => '@inheritDoc',
+                'insertText' => '@inheritDoc$0',
+            ],
+
+            [
+                'name'       => '@internal',
+                'insertText' => '@internal ${1:description}$0',
+            ],
+
+            [
+                'name'       => '@license',
+                'insertText' => '@license ${1:[url]} ${2:name}$0',
+            ],
+
+            [
+                'name'       => '@link',
+                'insertText' => '@link ${1:uri} ${2:[description]}$0',
+            ],
+
+            [
+                'name'       => '@method',
+                'insertText' => '@method ${1:type} ${2:name}(${3:[parameter list]})$0',
+            ],
+
+            [
+                'name'       => '@package',
+                'insertText' => '@package ${1:package name}$0',
+            ],
+
+            [
+                'name'       => '@param',
+                'insertText' => '@param ${1:mixed} \$${2:parameter} ${3:[description]}$0',
+            ],
+
+            [
+                'name'       => '@property',
+                'insertText' => '@property ${1:type} ${2:name} ${3:[description]}$0',
+            ],
+
+            [
+                'name'       => '@property-read',
+                'insertText' => '@property-read ${1:type} ${2:name} ${3:[description]}$0',
+            ],
+
+            [
+                'name'       => '@property-write',
+                'insertText' => '@property-write ${1:type} ${2:name} ${3:[description]}$0',
+            ],
+
+            [
+                'name'       => '@return',
+                'insertText' => '@return ${1:type} ${2:[description]}$0',
+            ],
+
+            [
+                'name'       => '@see',
+                'insertText' => '@see ${1:URI or FQSEN} ${2:description}$0',
+            ],
+
+            [
+                'name'       => '@since',
+                'insertText' => '@since ${1:version} ${2:[description]}$0',
+            ],
+
+            [
+                'name'       => '@source',
+                'insertText' => '@source ${1:start line} ${2:number of lines} ${3:[description]}$0',
+            ],
+
+            [
+                'name'       => '@throws',
+                'insertText' => '@throws ${1:exception type} ${2:[description]}$0',
+            ],
+
+            [
+                'name'       => '@todo',
+                'insertText' => '@todo ${1:description}$0',
+            ],
+
+            [
+                'name'       => '@uses',
+                'insertText' => '@uses ${1:FQSEN} ${2:[description]}$0',
+            ],
+
+            [
+                'name'       => '@var',
+                'insertText' => '@var ${1:type} ${2:\$${3:[property]} ${4:[description]}}$0',
+            ],
+
+            [
+                'name'       => '@version',
+                'insertText' => '@version ${1:vector} ${2:[description]}$0',
+            ],
         ];
     }
 }
