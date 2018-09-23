1727373e06670187077df12a3b6aa2b656aa3c7f
Gert <Gert-dev@users.noreply.github.com>
2018-09-23 20:16:26 +0200

    Remove deprecated and now unused getByteOffsetFromCharacterOffset
    Fixes #198

--- src/UserInterface/Command/AbstractCommand.php
+++ src/UserInterface/Command/AbstractCommand.php
@@ -7,20 +7,4 @@ namespace Serenata\UserInterface\Command;
  */
 abstract class AbstractCommand implements CommandInterface
 {
-    /**
-     * Retrieves the byte offset from the specified character offset in the specified string. The result will always be
-     * larger than or equal to the passed in value, depending on the amount of multi-byte characters encountered.
-     *
-     * @param int    $characterOffset
-     * @param string $string
-     *
-     * @deprecated All new commands should only support Position objects containing character offsets as per the
-     *             language server protocol. Only kept for backwards compatibility.
-     *
-     * @return int
-     */
-    final protected function getByteOffsetFromCharacterOffset(int $characterOffset, string $string): int
-    {
-        return strlen(mb_substr($string, 0, $characterOffset));
-    }
 }
