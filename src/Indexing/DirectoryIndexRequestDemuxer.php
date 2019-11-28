6620b1119480c3427fb5abc003f7cdaa5da336ba
Gert <Gert-dev@users.noreply.github.com>
2019-11-28 21:16:09 +0100

    Fix PHPCS errors

--- src/Indexing/DirectoryIndexRequestDemuxer.php
+++ src/Indexing/DirectoryIndexRequestDemuxer.php
@@ -119,102 +119,102 @@ final class DirectoryIndexRequestDemuxer
         $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
     }
 
-    /**
-     * @param string                        $token
-     * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
-     */
-    private function queueWorkDoneProgressCreateTokenRequest(
-        string $token,
-        JsonRpcMessageSenderInterface $jsonRpcMessageSender
-    ): void {
-        $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
-            'message' => new JsonRpcRequest(uniqid('IndexingTokenCreation'), 'window/workDoneProgress/create', [
-                'workDoneToken' => $token,
-            ]),
-        ]);
+    // /**
+    //  * @param string                        $token
+    //  * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    //  */
+    // private function queueWorkDoneProgressCreateTokenRequest(
+    //     string $token,
+    //     JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    // ): void {
+    //     $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
+    //         'message' => new JsonRpcRequest(uniqid('IndexingTokenCreation'), 'window/workDoneProgress/create', [
+    //             'workDoneToken' => $token,
+    //         ]),
+    //     ]);
 
-        $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
-    }
+    //     $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
+    // }
 
-    /**
-     * @param string                        $token
-     * @param string                        $folderUri
-     * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
-     */
-    private function queueWorkDoneProgressBeginNotification(
-        string $token,
-        string $folderUri,
-        JsonRpcMessageSenderInterface $jsonRpcMessageSender
-    ): void {
-        $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
-            'message' => new JsonRpcRequest(null, '$/progress', [
-                'token' => $token,
-                'value' => [
-                    'kind'        => 'begin',
-                    'title'       => 'Indexing (scanning)',
-                    'cancellable' => false,
-                    'message'     => "Indexing " . $folderUri . ' (' . number_format(0, 2) . ' %)',
-                    'percentage'  => 0.00,
-                ],
-            ]),
-        ]);
+    // /**
+    //  * @param string                        $token
+    //  * @param string                        $folderUri
+    //  * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    //  */
+    // private function queueWorkDoneProgressBeginNotification(
+    //     string $token,
+    //     string $folderUri,
+    //     JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    // ): void {
+    //     $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
+    //         'message' => new JsonRpcRequest(null, '$/progress', [
+    //             'token' => $token,
+    //             'value' => [
+    //                 'kind'        => 'begin',
+    //                 'title'       => 'Indexing (scanning)',
+    //                 'cancellable' => false,
+    //                 'message'     => "Indexing " . $folderUri . ' (' . number_format(0, 2) . ' %)',
+    //                 'percentage'  => 0.00,
+    //             ],
+    //         ]),
+    //     ]);
 
-        $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
-    }
+    //     $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
+    // }
 
-    /**
-     * @param string                        $token
-     * @param string                        $folderUri
-     * @param int                           $index
-     * @param int                           $total
-     * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
-     */
-    private function queueWorkDoneProgressReportNotification(
-        string $token,
-        string $folderUri,
-        int $index,
-        int $total,
-        JsonRpcMessageSenderInterface $jsonRpcMessageSender
-    ): void {
-        $progressPercentage = ($index / $total) * 100;
+    // /**
+    //  * @param string                        $token
+    //  * @param string                        $folderUri
+    //  * @param int                           $index
+    //  * @param int                           $total
+    //  * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    //  */
+    // private function queueWorkDoneProgressReportNotification(
+    //     string $token,
+    //     string $folderUri,
+    //     int $index,
+    //     int $total,
+    //     JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    // ): void {
+    //     $progressPercentage = ($index / $total) * 100;
 
-        $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
-            'message' => new JsonRpcRequest(null, '$/progress', [
-                'token' => $token,
-                'value' => [
-                    'kind'        => 'report',
-                    'cancellable' => false,
-                    'message'     => "Indexing " . $folderUri . ' (' . number_format($progressPercentage, 2) . ' %)',
-                    'percentage'  => $progressPercentage,
-                ],
-            ]),
-        ]);
+    //     $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
+    //         'message' => new JsonRpcRequest(null, '$/progress', [
+    //             'token' => $token,
+    //             'value' => [
+    //                 'kind'        => 'report',
+    //                 'cancellable' => false,
+    //                 'message'     => "Indexing " . $folderUri . ' (' . number_format($progressPercentage, 2) . ' %)',
+    //                 'percentage'  => $progressPercentage,
+    //             ],
+    //         ]),
+    //     ]);
 
-        $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
-    }
+    //     $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
+    // }
 
-    /**
-     * @param string                        $token
-     * @param string                        $folderUri
-     * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
-     */
-    private function queueWorkDoneProgressEndNotification(
-        string $token,
-        string $folderUri,
-        JsonRpcMessageSenderInterface $jsonRpcMessageSender
-    ): void {
-        $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
-            'message' => new JsonRpcRequest(null, '$/progress', [
-                'token' => $token,
-                'value' => [
-                    'kind'    => 'end',
-                    'message' => "Indexing " . $folderUri . ' was completed',
-                ],
-            ]),
-        ]);
+    // /**
+    //  * @param string                        $token
+    //  * @param string                        $folderUri
+    //  * @param JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    //  */
+    // private function queueWorkDoneProgressEndNotification(
+    //     string $token,
+    //     string $folderUri,
+    //     JsonRpcMessageSenderInterface $jsonRpcMessageSender
+    // ): void {
+    //     $request = new JsonRpcRequest(null, 'serenata/internal/echoMessage', [
+    //         'message' => new JsonRpcRequest(null, '$/progress', [
+    //             'token' => $token,
+    //             'value' => [
+    //                 'kind'    => 'end',
+    //                 'message' => "Indexing " . $folderUri . ' was completed',
+    //             ],
+    //         ]),
+    //     ]);
 
-        $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
-    }
+    //     $this->queue->push(new JsonRpcQueueItem($request, $jsonRpcMessageSender));
+    // }
 
     /**
      * Queues the legacy serenata/didProgressIndexing message.
